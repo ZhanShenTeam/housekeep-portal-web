9381c6f688382d76914f3a5aa83d30ca266aa21d
keson111 <928978147@qq.com>
2020-03-03 00:35:29 +0800

    更新了注册和登录

--- js/Marcel.js
+++ js/Marcel.js
@@ -48,3 +48,12 @@ function formatPrice(val) {
         }
     }
 }
+
+function  getUrlParam(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return decodeURI(r[2]);
+        }
+        return "";
+}
